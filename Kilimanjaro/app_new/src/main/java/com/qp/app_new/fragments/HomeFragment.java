f835b38b67bee5c5178eabbfb4faf164ce950df0
yangbin <121965871@qq.com>
2017-11-13 10:09:20 +0800

    替换logo

--- Kilimanjaro/app_new/src/main/java/com/qp/app_new/fragments/HomeFragment.java
+++ Kilimanjaro/app_new/src/main/java/com/qp/app_new/fragments/HomeFragment.java
@@ -4,17 +4,19 @@ import android.app.Dialog;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 
 import com.qp.app_new.AppPrefs;
 import com.qp.app_new.R;
 import com.qp.app_new.adapters.GameListAdapter;
-import com.qp.app_new.contents.AppPrefsContent;
 import com.qp.app_new.dialogs.DialogHelp;
 import com.qp.app_new.httpnetworks.NetWorkManager;
 import com.qp.app_new.interfaces.NetListener;
 import com.qp.app_new.interfaces.NormalDialogListener1;
+import com.qp.app_new.interfaces.PopupWindowItemListener;
 import com.qp.app_new.utils.ActivityStartUtils;
 import com.qp.app_new.utils.AppUtils;
+import com.qp.app_new.utils.ToastUtil;
 
 import org.json.JSONArray;
 
@@ -25,50 +27,59 @@ import org.json.JSONArray;
 public class HomeFragment extends BaseFragment implements View.OnClickListener {
 
     @Override
-    public void setContentView () {
+    public void setContentView() {
         layoutId = R.layout.fragment_home;
     }
 
     private ListView mGameListLV;
     private GameListAdapter mGameListAdapter;
     private Dialog mDialogQQ, mDialogPhone;
+    private PopupWindow mPopupWindow;
 
     @Override
-    public void initView () {
-        initActionBar ();
+    public void initView() {
+        findViewById(R.id.btn_left).setOnClickListener(this);
+        findViewById(R.id.qq_btn).setOnClickListener(this);
+        findViewById(R.id.phone_btn).setOnClickListener(this);
 
-        findViewById (R.id.qq_btn).setOnClickListener (this);
-        findViewById (R.id.phone_btn).setOnClickListener (this);
-
-        mGameListLV = (ListView) findViewById (R.id.game_list_lv);
-        mGameListAdapter = new GameListAdapter ();
-        mGameListLV.setAdapter (mGameListAdapter);
-
-        mGameListLV.setOnItemClickListener (new AdapterView.OnItemClickListener () {
+        final String[] popList = {getString(R.string.hall_paying_method)};
+        mPopupWindow = DialogHelp.createPopupWindow(getActivity(), popList, new PopupWindowItemListener() {
             @Override
-            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
-                if (judgeLogin ()) {
+            public void onPopItemClick(View v, int position) {
+                ToastUtil.showToast(popList[position]);
+                mPopupWindow.dismiss();
+            }
+        });
+
+        mGameListLV = (ListView) findViewById(R.id.game_list_lv);
+        mGameListAdapter = new GameListAdapter();
+        mGameListLV.setAdapter(mGameListAdapter);
+
+        mGameListLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (judgeLogin()) {
                     // 去游戏页面
-                    ActivityStartUtils.startGameActivity (getActivity (), mGameListAdapter.getItem (position));
+                    ActivityStartUtils.startGameActivity(getActivity(), mGameListAdapter.getItem(position));
                 }
             }
         });
     }
 
     @Override
-    public void onResume () {
-        super.onResume ();
-        updateData ();
+    public void onResume() {
+        super.onResume();
+        updateData();
     }
 
-    public void updateData () {
-        if (AppPrefsContent.isLogined ()) {
-            setTitle (AppPrefsContent.getUser ().optString ("nickName"));
-        } else {
-            setTitle (R.string.text_yk);
-        }
-        if (mGameListAdapter.getCount () == 0) {
-            getGameList ();
+    public void updateData() {
+//        if (AppPrefsContent.isLogined ()) {
+//            setTitle (AppPrefsContent.getUser ().optString ("nickName"));
+//        } else {
+//            setTitle (R.string.text_yk);
+//        }
+        if (mGameListAdapter.getCount() == 0) {
+            getGameList();
         }
     }
 
@@ -77,52 +88,57 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
     /**
      * 获取游戏列表
      */
-    private void getGameList () {
-        NetWorkManager.getInstance ().getGameList (mLoadingDialog, new NetListener () {
+    private void getGameList() {
+        NetWorkManager.getInstance().getGameList(mLoadingDialog, new NetListener() {
             @Override
-            public void onSuccessResponse (String msg, JSONArray jsonArray) {
-                super.onSuccessResponse (msg, jsonArray);
-                mGameListAdapter.setJSONArray (jsonArray);
+            public void onSuccessResponse(String msg, JSONArray jsonArray) {
+                super.onSuccessResponse(msg, jsonArray);
+                mGameListAdapter.setJSONArray(jsonArray);
             }
 
             @Override
-            public void onErrorResponse (int errorWhat, String message) {
+            public void onErrorResponse(int errorWhat, String message) {
             }
         });
     }
 
     @Override
-    public void onClick (View v) {
-        switch (v.getId ()) {
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_left:
+                if (!mPopupWindow.isShowing()) {
+                    mPopupWindow.showAsDropDown(v, 0, 0);
+                }
+                break;
             case R.id.qq_btn:
-                if (!AppUtils.isQQClientAvailable (getActivity ())) {
-                    DialogHelp.showMessageDialog (getActivity (), getString (R.string.recharge_hasnot_qq));
+                if (!AppUtils.isQQClientAvailable(getActivity())) {
+                    DialogHelp.showMessageDialog(getActivity(), getString(R.string.recharge_hasnot_qq));
                 } else {
-                    final String kefu_qq_num_default = AppPrefs.getInstance ().getSysInfoQQ ();
-                    String call_kefu_qq = getString (R.string.call_kefu_qq, kefu_qq_num_default);
+                    final String kefu_qq_num_default = AppPrefs.getInstance().getSysInfoQQ();
+                    String call_kefu_qq = getString(R.string.call_kefu_qq, kefu_qq_num_default);
                     if (mDialogQQ == null) {
-                        mDialogQQ = DialogHelp.createOkDialog (getActivity (), call_kefu_qq, new NormalDialogListener1 () {
+                        mDialogQQ = DialogHelp.createOkDialog(getActivity(), call_kefu_qq, new NormalDialogListener1() {
                             @Override
-                            public void onOkClickListener () {
-                                AppUtils.intoQQ (getActivity (), kefu_qq_num_default);
+                            public void onOkClickListener() {
+                                AppUtils.intoQQ(getActivity(), kefu_qq_num_default);
                             }
                         });
                     }
-                    mDialogQQ.show ();
+                    mDialogQQ.show();
                 }
                 break;
             case R.id.phone_btn:
                 if (mDialogPhone == null) {
-                    final String kefu_phone_default = AppPrefs.getInstance ().getSysInfoPhone ();
-                    mDialogPhone = DialogHelp.createOkDialog (getActivity (), getString (R.string.app_call_phone, kefu_phone_default), new
-                            NormalDialogListener1 () {
+                    final String kefu_phone_default = AppPrefs.getInstance().getSysInfoPhone();
+                    mDialogPhone = DialogHelp.createOkDialog(getActivity(), getString(R.string.app_call_phone, kefu_phone_default), new
+                            NormalDialogListener1() {
                                 @Override
-                                public void onOkClickListener () {
-                                    AppUtils.call (getActivity (), kefu_phone_default);
+                                public void onOkClickListener() {
+                                    AppUtils.call(getActivity(), kefu_phone_default);
                                 }
                             });
                 }
-                mDialogPhone.show ();
+                mDialogPhone.show();
                 break;
         }
     }
--- Kilimanjaro/app_new/src/main/java/com/qp/app_new/fragments/RankingFragment.java
+++ Kilimanjaro/app_new/src/main/java/com/qp/app_new/fragments/RankingFragment.java
@@ -9,7 +9,6 @@ import android.widget.TextView;
 
 import com.qp.app_new.R;
 import com.qp.app_new.adapters.RankingAdapter;
-import com.qp.app_new.configs.NetStatusConfig;
 import com.qp.app_new.contents.AppPrefsContent;
 import com.qp.app_new.dialogs.DialogHelp;
 import com.qp.app_new.httpnetworks.NetWorkManager;
@@ -17,7 +16,6 @@ import com.qp.app_new.interfaces.NetListener;
 import com.qp.app_new.interfaces.PopupWindowItemListener;
 import com.qp.app_new.utils.ActivityStartUtils;
 import com.qp.app_new.utils.StringUtil;
-import com.qp.app_new.utils.ToastUtil;
 
 import org.json.JSONArray;
 
@@ -31,7 +29,7 @@ import java.util.HashMap;
 public class RankingFragment extends BaseFragment implements View.OnClickListener {
 
     @Override
-    public void setContentView () {
+    public void setContentView() {
         layoutId = R.layout.fragment_ranking;
     }
 
@@ -49,39 +47,39 @@ public class RankingFragment extends BaseFragment implements View.OnClickListene
     private Runnable mRunnable;
 
     @Override
-    public void initView () {
-        mPopList = new String[]{getString (R.string.bottom_tab_order_d),
-                getString (R.string.bottom_tab_order_w),
-                getString (R.string.bottom_tab_order_m)};
-        initActionBar ();
-        setRightIV (R.drawable.ic_menu);
-        setTitle (mPopList[mPosition]);
+    public void initView() {
+        mPopList = new String[]{getString(R.string.bottom_tab_order_d),
+                getString(R.string.bottom_tab_order_w),
+                getString(R.string.bottom_tab_order_m)};
+        initActionBar();
+        setRightIV(R.drawable.ic_menu);
+        setTitle(mPopList[mPosition]);
 
-        mPopupWindow = DialogHelp.createPopupWindow (getActivity (), mPopList, new PopupWindowItemListener () {
+        mPopupWindow = DialogHelp.createPopupWindow(getActivity(), mPopList, new PopupWindowItemListener() {
             @Override
-            public void onPopItemClick (View v, int position) {
+            public void onPopItemClick(View v, int position) {
                 mPosition = position;
-                setTitle (mPopList[mPosition]);
-                updateData ();
-                mPopupWindow.dismiss ();
+                setTitle(mPopList[mPosition]);
+                updateData();
+                mPopupWindow.dismiss();
             }
         });
 
-        mRankingLV = (ListView) findViewById (R.id.lv_ranking);
-        mRankingAdapter = new RankingAdapter ();
-        mRankingLV.setAdapter (mRankingAdapter);
+        mRankingLV = (ListView) findViewById(R.id.lv_ranking);
+        mRankingAdapter = new RankingAdapter();
+        mRankingLV.setAdapter(mRankingAdapter);
 
-        mRankingView = findViewById (R.id.ly_ranking);
-        mErrorView = findViewById (R.id.error_ly);
-        mErrorView.setOnClickListener (this);
-        mErrorTV = (TextView) findViewById (R.id.error_tv);
+        mRankingView = findViewById(R.id.ly_ranking);
+        mErrorView = findViewById(R.id.error_ly);
+        mErrorView.setOnClickListener(this);
+        mErrorTV = (TextView) findViewById(R.id.error_tv);
 
-        mHandler = new Handler ();
-        mRunnable = new Runnable () {
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
             @Override
-            public void run () {
-                updateData ();
-                mHandler.postDelayed (mRunnable, 1000 * 60 * 1);// 一分钟刷新一次
+            public void run() {
+                updateData();
+                mHandler.postDelayed(mRunnable, 1000 * 60 * 1);// 一分钟刷新一次
             }
         };
     }
@@ -100,85 +98,87 @@ public class RankingFragment extends BaseFragment implements View.OnClickListene
 //    };
 
     @Override
-    public void onResume () {
-        super.onResume ();
-        mHandler.post (mRunnable);
+    public void onResume() {
+        super.onResume();
+        mHandler.post(mRunnable);
     }
 
     @Override
-    public void onPause () {
-        super.onPause ();
-        mHandler.removeCallbacks (mRunnable);
+    public void onPause() {
+        super.onPause();
+        mHandler.removeCallbacks(mRunnable);
     }
 
     // 界面刷新数据(有数据时，不需要)
-    public void updateData () {
+    public void updateData() {
         mJSONArray = null;
         // 判断是否有登录
-        if (!AppPrefsContent.isLogined ()) {
-            dataError (getString (R.string.click_login));
+        if (!AppPrefsContent.isLogined()) {
+            dataError(getString(R.string.click_login));
             return;
         }
 
-        mErrorTV.setText (getString (R.string.app_loading));
-        getOrderList ();
+        mErrorTV.setText(getString(R.string.app_loading));
+        getOrderList();
     }
 
     // ************************************
-    private void getOrderList () {// 在此方法中需要顺便判断Token是否过期
+    private void getOrderList() {// 在此方法中需要顺便判断Token是否过期
         // 判断网络返回数据
-        HashMap<String, Object> agrs = new HashMap<> ();
-        agrs.put ("type", mPosition + 1);// //类型 1:日排行 2:周排行 3:月排行  五分钟刷新一次数据
-        NetWorkManager.getInstance ().getGameRankingList (StringUtil.getJson (agrs), mDialog, new NetListener () {
+        HashMap<String, Object> agrs = new HashMap<>();
+        agrs.put("type", mPosition + 1);// //类型 1:日排行 2:周排行 3:月排行  五分钟刷新一次数据
+        NetWorkManager.getInstance().getGameRankingList(StringUtil.getJson(agrs), mDialog, new NetListener() {
             @Override
-            public void onSuccessResponse (String msg, JSONArray jsonArray) {
-                super.onSuccessResponse (msg, jsonArray);
+            public void onSuccessResponse(String msg, JSONArray jsonArray) {
+                super.onSuccessResponse(msg, jsonArray);
                 mDialog = mLoadingDialog;
                 if (mJSONArray == null) {
-                    mJSONArray = new JSONArray ();
+                    mJSONArray = new JSONArray();
                     mJSONArray = jsonArray;
                 } else {
-                    for (int i = 0; i < jsonArray.length (); i++) {
-                        mJSONArray.put (jsonArray.optJSONObject (i));
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        mJSONArray.put(jsonArray.optJSONObject(i));
                     }
                 }
 
                 // 设置到adapter
-                mRankingView.setVisibility (View.VISIBLE);
-                mErrorView.setVisibility (View.GONE);
-                mRankingAdapter.setJSONArray (mJSONArray);
+                mRankingView.setVisibility(View.VISIBLE);
+                mErrorView.setVisibility(View.GONE);
+                mRankingAdapter.setJSONArray(mJSONArray);
             }
 
             @Override
-            public void onErrorResponse (int errorWhat, String message) {
+            public void onErrorResponse(int errorWhat, String message) {
                 mDialog = mLoadingDialog;
-                dataError (message);
+                dataError(message);
             }
         });
     }
 
     @Override
-    public void onClick (View v) {
-        switch (v.getId ()) {
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.error_ly:
-                if (mErrorTV.getText ().toString ().equals (getString (R.string.click_login))) {
-                    ActivityStartUtils.startLoginActivity (getActivity ());
+                if (mErrorTV.getText().toString().equals(getString(R.string.click_login))) {
+                    ActivityStartUtils.startLoginActivity(getActivity());
                 } else {// 无数据  刷新
-                    updateData ();
+                    updateData();
                 }
                 break;
         }
     }
 
     @Override
-    public void onRightClick (View v) {
-        if (!mPopupWindow.isShowing ()) mPopupWindow.showAsDropDown (v, 0, 0);
+    public void onRightClick(View v) {
+        if (!mPopupWindow.isShowing()) {
+            mPopupWindow.showAsDropDown(v, 0, 0);
+        }
     }
 
-    private void dataError (String message) {
-        mErrorTV.setText (message);
+    private void dataError(String message) {
+        mErrorTV.setText(message);
         mJSONArray = null;
-        mRankingView.setVisibility (View.GONE);
-        mErrorView.setVisibility (View.VISIBLE);
+        mRankingView.setVisibility(View.GONE);
+        mErrorView.setVisibility(View.VISIBLE);
     }
 }
